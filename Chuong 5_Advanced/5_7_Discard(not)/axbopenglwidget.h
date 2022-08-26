9fc3ec8339d627f8d53f474d147c4a3445fe9416
Huy Nguyễn <dreamofbaby0507@gmail.com>
2022-08-26 08:38:34 +0700

    update face culling

---
+++ Chuong 5_Advanced/5_7_Discard(not)/axbopenglwidget.h
@@ -0,0 +1,43 @@
+#ifndef AXBOPENGLWIDGET_H
+#define AXBOPENGLWIDGET_H
+
+#include <QOpenGLWidget>
+#include <QOpenGLFunctions_3_3_Core>
+#include "camera.h"
+#include <QPointF>
+#include <QOpenGLShaderProgram>
+#include <QTimer>
+#include <QOpenGLTexture>
+#include "mesh.h"
+
+
+class AXBOpenGLWidget : public QOpenGLWidget,QOpenGLFunctions_3_3_Core
+{
+    Q_OBJECT
+public:
+    explicit AXBOpenGLWidget(QWidget *parent = nullptr);
+
+private slots:
+    void on_TimeOut();
+protected:
+    void initializeGL();
+    void resizeGL(int w, int h);
+    void paintGL();
+    // Event
+    void mousePressEvent(QMouseEvent *event);
+    void mouseMoveEvent(QMouseEvent *event);
+    void wheelEvent(QWheelEvent *event);
+    void keyPressEvent(QKeyEvent *event);
+private:
+    Camera m_camera;
+    QOpenGLShaderProgram shaderProgram, lightshaderProgram, floorshaderProgram;
+    QTimer m_timer;
+    QMatrix4x4 projection,view,model,modelLight;
+    QOpenGLTexture *texDiffuse;
+    QOpenGLTexture *texSpecular;
+    Mesh *cubeMesh;
+    Mesh *planeMesh;
+    Mesh * processMesh(float *vertices, int size , unsigned int textureId);
+};
+
+#endif // AXBOPENGLWIDGET_H
